155c1b76ac6f51a8be55dcc5fc91aa966dcae396
NutInSpace <134748270+NutInSpace@users.noreply.github.com>
2023-06-01 22:49:21 -0500

    Update reference.lua
    Here's the refactored code:
    
    lua
    Copy code
    -- Duplicate (requires link to core)
    -- Purpose: Find every item on a core.
    -- Args: Filter (Boolean) If only one exists then duplicate. This avoids duplicating the factory elements. Default True
    -- Args: Maintain (integer) How many to make. If 0, then use element count on core.
    
    function build_recipe_table(filter, maintain)
      recipes = {}
    
      if core == nil then
        system.print("Missing link to core!")
      else
        system.print("Duplicating:")
      end
    
      -- Find everything on the core
      core_items = {}
      for _, element in pairs(core.getElementIdList()) do
        item = core.getElementItemIdById(element)
    
        -- track how many we want to transfer
        core_items[item] = (core_items[item] or 0) + 1
      end
    
      -- Build Recipe List
      for item, qty in pairs(core_items) do
        if maintain ~= 0 then
          qty = maintain
        end
        if filter and qty == 1 then
          table.insert(recipes, { id = item, quantity = qty })
          system.print(getName(item) .. " -> " .. qty)
        end
      end
    
      return recipes
    end
    Here are the changes made:
    
    Removed unnecessary empty line and excessive indentation.
    Removed redundant parentheses in the if conditions.
    Consolidated the declaration of the core_items table.
    Simplified the tracking of item quantities using the or operator.
    Updated the getName function, assuming it's defined elsewhere in the code.

--- reference.lua
+++ reference.lua
@@ -1,41 +1,36 @@
---
 -- Duplicate (requires link to core)
--- Purpose: Find every item on a core. 
+-- Purpose: Find every item on a core.
 -- Args: Filter (Boolean) If only one exists then duplicate. This avoids duplicating the factory elements. Default True
 -- Args: Maintain (integer) How many to make. If 0, then use element count on core.
 
 function build_recipe_table(filter, maintain)
   recipes = {}
-  
-  if (core == nil) then
-      system.print("missing link to core!")
+
+  if core == nil then
+    system.print("Missing link to core!")
   else
-      system.print("duplicating:")
+    system.print("Duplicating:")
   end
 
   -- Find everything on the core
-  elements_by_id = core.getElementIdList()
   core_items = {}
-  for _,element in pairs(elements_by_id) do
-      item = core.getElementItemIdById(element)
-    
-      -- track how many we want to transfer
-      if (core_items[item] == nil) then
-         core_items[item] = 1
-      else
-         core_items[item] = core_items[item] + 1
-      end
+  for _, element in pairs(core.getElementIdList()) do
+    item = core.getElementItemIdById(element)
+
+    -- track how many we want to transfer
+    core_items[item] = (core_items[item] or 0) + 1
   end
 
   -- Build Recipe List
-  for item,qty in pairs(core_items) do
-      if (maintain ~= 0) then
-         qty = maintain
-      end
-      if (filter == true) and (qty == 1) then
-          table.insert(recipes, {id = item, quantity = qty})
-          system.print(getName(item).." -> "..qty)
-      end
+  for item, qty in pairs(core_items) do
+    if maintain ~= 0 then
+      qty = maintain
+    end
+    if filter and qty == 1 then
+      table.insert(recipes, { id = item, quantity = qty })
+      system.print(getName(item) .. " -> " .. qty)
+    end
   end
+
   return recipes
 end
