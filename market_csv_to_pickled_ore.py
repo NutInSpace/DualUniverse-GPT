bc961c08c8f3305a2ec2b6479060592ac968a46f
NutInSpace <134748270+NutInSpace@users.noreply.github.com>
2023-07-01 21:35:20 -0500

    Update market_csv_to_pickled_ore.py
    Add optimization args via argparse

--- market_csv_to_pickled_ore.py
+++ market_csv_to_pickled_ore.py
@@ -1,6 +1,9 @@
+import argparse
+
 import concurrent.futures
 import pandas as pd
 import seaborn as sns
+
 from datetime import datetime
 from matplotlib import pyplot as plt
 from matplotlib.dates import DateFormatter
@@ -18,8 +21,8 @@ class DataProcessor:
             return '${:,.2f}'.format(x)
 
         self.currency_formatter = FuncFormatter(currency)
-        
-    def simple_plot(self, buy_orders, sell_orders, num_days=10, bid_min=1, bid_max=100, show=True):
+
+    def simple_plot(self, buy_orders, sell_orders, num_days=10, bid_min=1, bid_max=1000000, show=True):
         # Convert 'expiration_date' to datetime type, for testing old data apply an offset
         buy_orders['expiration_date'] = pd.to_datetime(buy_orders['expiration_date']) # + pd.DateOffset(days=30)
         sell_orders['expiration_date'] = pd.to_datetime(sell_orders['expiration_date'])
@@ -30,7 +33,7 @@ class DataProcessor:
 
         # Combine the two dataframes
         combined = pd.concat([buy_orders, sell_orders])
-        
+
         # Now we have df
         df = combined
 
@@ -46,6 +49,12 @@ class DataProcessor:
         expiration_limit = current_time - pd.DateOffset(days=num_days)
         df = df[df['expiration_date'] > expiration_limit]
         df['expires_in_days'] = df['expiration_date'] - current_time
+
+        # Get the ore tier
+        df['ore_tier'] = df['item_name'].map(ore_tiers)
+
+        # Adjust the bid_max based on the ore tier
+        df['bid_max'] = df['ore_tier'].map({1: bid_max, 2: bid_max * 2, 3: bid_max * 3})
 
         # Create a barplot
         plt.figure(figsize=(12, 6))
@@ -66,7 +75,7 @@ class DataProcessor:
 
         return df
 
-def process_item(item, ParseLogs=True):
+def process_item(item, ParseLogs=True, ShowPlots=True):
     # Parse Log Files
     if ParseLogs:
         LogParser(item_name=item)
@@ -78,30 +87,57 @@ def process_item(item, ParseLogs=True):
     buy_orders = pd.read_csv(f'data\\csv\\buy_{item}_market_orders.csv')
     sell_orders = pd.read_csv(f'data\\csv\\sell_{item}_market_orders.csv')
 
+    # Get the ore tier
+    ore_tier = ore_tiers.get(item, 1)
+
+    # Determine the bid_max based on the ore tier
+    _max = 5000
+    if ore_tier == 1:
+        _max = 250
+    elif ore_tier == 2:
+        _max = 500
+        
     # Razzle Dazzle
-    df = data_processor.simple_plot(buy_orders, sell_orders, num_days=10, bid_min=1, bid_max=100, show=False)
+    df = data_processor.simple_plot(buy_orders, sell_orders, num_days=10, bid_min=1, bid_max=_max, show=ShowPlots)
 
     # Pickle the dataframe
     df.to_pickle(f'data\\pickle\\{item}.pickle')
 
+
 ## Main ###
 item_names = [
-    'Hematite', 'Quartz', 'Coal', 'Bauxite',
+    'Hematite', 'Quartz', 'Coal', 'Bauxite', 
     'Chromite', 'Limestone', 'Malachite',
     'Acanthite', 'Garnierite', 'Petalite', 'Pyrite',
     'Cobaltite', 'Cryolite', 'Gold_nuggets', 'Kolbeckite',
     'Columbite', 'Ilmenite', 'Rhodonite', 'Vanadinite'
 ]
 
+# Define the ore tiers
+ore_tiers = {
+    'Hematite': 1, 'Quartz': 1, 'Coal': 1, 'Bauxite': 1,
+    'Chromite': 2, 'Limestone': 2, 'Malachite': 2,
+    'Acanthite': 3, 'Garnierite': 3, 'Petalite': 3, 'Pyrite': 3,
+    'Cobaltite': 4, 'Cryolite': 4, 'Gold_nuggets': 4, 'Kolbeckite': 4,
+    'Columbite': 5, 'Ilmenite': 5, 'Rhodonite': 5, 'Vanadinite': 5
+}
+
+# Argument parsing
+parser = argparse.ArgumentParser(description='Process item data.')
+parser.add_argument('--no-parse-logs', dest='parse_logs', action='store_false', help='Do not parse log files')
+parser.add_argument('--no-show-plots', dest='show_plots', action='store_false', help='Do not show plots')
+parser.set_defaults(parse_logs=True, show_plots=True)
+args = parser.parse_args()
+
 # Create a ThreadPoolExecutor
 MultiThread = False
 if MultiThread:
     with concurrent.futures.ThreadPoolExecutor() as executor:
         # Submit tasks for each item
-        futures = [executor.submit(process_item, item) for item in item_names]
+        futures = [executor.submit(process_item, item, args.parse_logs, args.show_plots) for item in item_names]
 
         # Wait for all tasks to complete
         concurrent.futures.wait(futures)
 else:
     for _item in item_names:
-        process_item(_item)
+        process_item(_item, args.parse_logs, args.show_plots)
