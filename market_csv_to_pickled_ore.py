63eca39ba93c82bb7003a8ef4b07775eaa772baf
NutInSpace <134748270+NutInSpace@users.noreply.github.com>
2023-07-04 13:11:53 -0500

    Update market_csv_to_pickled_ore.py
    Add some stats on the charts such as the market price means

--- market_csv_to_pickled_ore.py
+++ market_csv_to_pickled_ore.py
@@ -10,6 +10,7 @@ from matplotlib.dates import DateFormatter
 from matplotlib.ticker import FuncFormatter
 from mplfinance.original_flavor import candlestick_ohlc
 from data_processing import LogParser # Rename to market_log_to_csv.py to data_processing.py
+from item_helper import ItemManager
 
 class DataProcessor:
     def __init__(self, item_name):
@@ -22,7 +23,10 @@ class DataProcessor:
 
         self.currency_formatter = FuncFormatter(currency)
 
-    def simple_plot(self, buy_orders, sell_orders, num_days=30, bid_min=1, bid_max=1000, show=True):
+        # Item Details
+        self.item_manager = ItemManager()
+
+    def process_data(self, buy_orders, sell_orders, num_days=30, bid_min=1, bid_max=2500):
         # Convert 'expiration_date' to datetime type, for testing old data apply an offset
         buy_orders['expiration_date'] = pd.to_datetime(buy_orders['expiration_date']) # + pd.DateOffset(days=30)
         sell_orders['expiration_date'] = pd.to_datetime(sell_orders['expiration_date'])
@@ -37,39 +41,75 @@ class DataProcessor:
         combined = pd.concat([buy_orders, sell_orders])
 
         # Now we have df
-        df = combined
+        self.df = combined
 
         # Scale for plotting
-        df['unit_price'] = df['unit_price'] / 100
+        self.df['unit_price'] = self.df['unit_price'] / 100
 
         # Filter Stupid Players
-        df = df[df['unit_price'] < bid_max]
-        df = df[df['unit_price'] > bid_min]
+        self.df = self.df[self.df['unit_price'] < bid_max]
+        self.df = self.df[self.df['unit_price'] > bid_min]
 
         # Filter expired orders
         current_time = pd.Timestamp.now()
         expiration_limit = current_time - pd.DateOffset(days=num_days)
-        df = df[df['expiration_date'] > expiration_limit]
-        df['expires_in_days'] = df['expiration_date'] - current_time
+        self.df = self.df[self.df['expiration_date'] > expiration_limit]
+        self.df['expires_in_days'] = self.df['expiration_date'] - current_time
 
         # Get the ore tier
-        df['ore_tier'] = df['item_name'].map(ore_tiers)
+        self.df['ore_tier'] = self.df['item_name'].map(ore_tiers)
 
         # Adjust the bid_max based on the ore tier
-        df['bid_max'] = df['ore_tier'].map({1: bid_max, 2: bid_max * 2, 3: bid_max * 3})
+        self.df['bid_max'] = self.df['ore_tier'].map({1: bid_max, 2: bid_max * 2, 3: bid_max * 3})
 
-        # Create a barplot
+        return self.df
+
+    def mean_buy_price(self):
+        return self.df[(self.df['order_type'] == 'Buy')]['unit_price'].mean()
+    
+    def mean_sell_price(self):
+        return self.df[(self.df['order_type'] == 'Sell')]['unit_price'].mean()
+
+    def highest_buy_order(self):
+        return self.df[(self.df['order_type'] == 'Buy')]['unit_price'].max()
+    
+    def lowest_sell_order(self):
+        return self.df[(self.df['order_type'] == 'Sell')]['unit_price'].min()
+
+    def mean_market_price(self):
+        return self.df['unit_price'].mean()
+
+    def calculate_market_mass(self, order_type):
+        #market_id_counts = self.df[self.df['order_type'] == order_type]['market_id'].value_counts()
+        #if not market_id_counts.empty:
+        #    most_common_market = market_id_counts.idxmax()
+            # total_value = self.df[(self.df['order_type'] == order_type) & (self.df['market_id'] == most_common_market)]['buy_quantity'].sum()
+        #    return total_value
+        #else:
+        #    return 0
+        
+        item_mass = self.item_manager.lookup_item_mass(self.item_name)
+        return self.df[(self.df['order_type'] == order_type)]['buy_quantity'].sum() * item_mass
+
+
+    def calculate_market_value(self, order_type):
+        #market_id_counts = self.df[self.df['order_type'] == order_type]['market_id'].value_counts()
+        #if not market_id_counts.empty:
+        #    most_common_market = market_id_counts.idxmax()
+            # total_value = self.df[(self.df['order_type'] == order_type) & (self.df['market_id'] == most_common_market)]['buy_quantity'].sum()
+        #    return total_value
+        #else:
+        #    return 0
+        return self.df[(self.df['order_type'] == order_type)]['buy_quantity'].sum() * self.mean_market_price()
+
+    def plot_data(self, show=True):
         plt.figure(figsize=(12, 6))
-        #ax = sns.barplot(data=df, x='expires_in_days', y='unit_price', hue='order_type')
-        ax = sns.barplot(data=df, x='buy_quantity', y='unit_price', hue='order_type')
+        ax = sns.barplot(data=self.df, x='buy_quantity', y='unit_price', hue='order_type')
         ax.yaxis.set_major_formatter(self.currency_formatter)
 
-        #date_format = DateFormatter("%d")
-        #ax.xaxis.set_major_formatter(date_format)
-
         # Set x-axis to logarithmic scale
-        sns.set_style("whitegrid")
-        #plt.xscale("log")
+        # sns.set_style("whitegrid")
+        # plt.xscale("log")
 
         # Setting the x-axis format
         plt.xlabel('Volume')
@@ -78,20 +118,48 @@ class DataProcessor:
         # Set the Grid
         ax.grid = True
 
-        # Limiting the number of x-axis labels5
-        x = df['buy_quantity']
+        # Limiting the number of x-axis labels
+        x = self.df['buy_quantity']
         max_labels = 10  # Maximum number of labels to show
         label_indices = range(0, len(x), len(x)//max_labels)
         plt.xticks(label_indices)
 
+        # Calculate and display total value for each order typef
+        buy_value = self.calculate_market_value('Buy')
+        sell_value = self.calculate_market_value('Sell')
+        sell_mass = self.calculate_market_mass('Sell')
+        buy_value /= 1000 # hundred-thousand
+        buy_value /= 1000 # millions
+        #buy_value /= 1000 # billions
+        sell_value /= 1000 # thousand
+        sell_value /= 1000 # million
+        #sell_value /= 1000 # billion
+        sell_mass /= 1000 # K
+        sell_mass /= 1000 # Meta
+        #sell_mass /= 1000 # Giga
+
+        # Maths
+        ## Summary
+        plt.text(0.75, 0.45, f'Lowest Sell Order: ${self.lowest_sell_order():,.2f}', transform=ax.transAxes, fontsize=12, fontweight='bold')
+        plt.text(0.75, 0.50, f'Highest Buy Order: ${self.highest_buy_order():,.2f}', transform=ax.transAxes, fontsize=12, fontweight='bold')
+        
+        plt.text(0.75, 0.35, f'Mean Buy Price ${self.mean_buy_price():,.2f}', transform=ax.transAxes, fontsize=10)
+        plt.text(0.75, 0.30, f'Mean Sell Price : ${self.mean_sell_price():,.2f}', transform=ax.transAxes, fontsize=10)
+        plt.text(0.75, 0.25, f'Mean Price (All) ${self.mean_market_price():,.2f}', transform=ax.transAxes, fontsize=10) 
+        plt.text(0.75, 0.20, f'Total Value (Buy): ${buy_value:,.2f}', transform=ax.transAxes, fontsize=10)
+        plt.text(0.75, 0.15, f'Total Value (Sell): ${sell_value:,.2f}', transform=ax.transAxes, fontsize=10)
+        plt.text(0.75, 0.10, f'Total Mass (Sell): {sell_mass:,.4f} tons', transform=ax.transAxes, fontsize=10)
+        plt.text(0.75, 0.05, f'Units in (Billions, Megatons)', transform=ax.transAxes, fontsize=10)
+
         # Adding labels and title
-        plt.title(f'All Markets : Unit price of Buy and Sell Orders : {self.item_name}')
+        today = datetime.today().strftime("%Y-%m-%d")
+        num_data_points = len(self.df)
+        plt.title(f'All Markets : Unit price of Buy and Sell Orders : {self.item_name}\nDate of Analysis: {today} Sample Pop : {num_data_points}')
+        
         if show:
             plt.show()
 
-        return df
-
-def process_item(item, ParseLogs=True, ShowPlots=True):
+def process_item(item, ParseLogs=True, ShowPlots=True, Pickle=False):
     # Parse Log Files
     if ParseLogs:
         LogParser(item_name=item)
@@ -100,7 +168,7 @@ def process_item(item, ParseLogs=True, ShowPlots=True):
     data_processor = DataProcessor(item)
 
     # Read Data
-    temp_name = item.replace(" ", "")
+    temp_name = item.replace(" ", "_")
     buy_orders = pd.read_csv(f'data\\csv\\buy_{temp_name}_market_orders.csv')
     sell_orders = pd.read_csv(f'data\\csv\\sell_{temp_name}_market_orders.csv')
 
@@ -113,21 +181,26 @@ def process_item(item, ParseLogs=True, ShowPlots=True):
         _max = 250
     elif ore_tier == 2:
         _max = 500
-        
+    
+    df = data_processor.process_data(buy_orders, sell_orders, num_days=10, bid_min=1, bid_max=_max)
+
     # Razzle Dazzle
-    df = data_processor.simple_plot(buy_orders, sell_orders, num_days=10, bid_min=1, bid_max=_max, show=ShowPlots)
+    if ShowPlots:
+        data_processor.plot_data()
 
     # Pickle the dataframe
-    df.to_pickle(f'data\\pickle\\{item}.pickle')
+    if Pickle:
+        temp_name = item.replace(" ", "_")
+        df.to_pickle(f'data\\pickle\\{temp_name}.pickle')
 
 
 ## Main ###
 item_names = [
-    'Hematite', 'Quartz', 'Coal', 'Bauxite', 
-    'Chromite', 'Limestone', 'Malachite',
+    'Columbite', 'Ilmenite', 'Rhodonite', 'Vanadinite',
+    'Cobaltite', 'Cryolite', 'Gold nuggets', 'Kolbeckite',
     'Acanthite', 'Garnierite', 'Petalite', 'Pyrite',
-    'Cobaltite', 'Cryolite', 'Gold Nuggets', 'Kolbeckite',
-    'Columbite', 'Ilmenite', 'Rhodonite', 'Vanadinite'
+    'Chromite', 'Limestone', 'Malachite',
+    'Hematite', 'Quartz', 'Coal', 'Bauxite'
 ]
 
 # Define the ore tiers
@@ -143,7 +216,7 @@ ore_tiers = {
 parser = argparse.ArgumentParser(description='Process item data.')
 parser.add_argument('--no-parse-logs', dest='parse_logs', action='store_false', help='Do not parse log files')
 parser.add_argument('--no-show-plots', dest='show_plots', action='store_false', help='Do not show plots')
-parser.set_defaults(parse_logs=True, show_plots=True)
+parser.set_defaults(parse_logs=False, show_plots=True)
 args = parser.parse_args()
 
 # Create a ThreadPoolExecutor
